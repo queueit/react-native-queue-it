908e36e18e4b0009ffb317da4b5c150eb16186c5
Vasil <vava@queue-it.com>
2020-07-21 15:37:06 +0200

    Update

--- ios/QueueIt.h
+++ ios/QueueIt.h
@@ -1,5 +1,18 @@
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTUtils.h>
+#import "QueueITEngine.h"
+#define ENQUEUE_STATE(x) \
+    (EnqueueResultState_toString[x])
 
-@interface QueueIt : NSObject <RCTBridgeModule>
+typedef enum {
+    Passed,
+    Disabled,
+    Unavailable
+} EnqueueResultState;
+
+extern NSString * const EnqueueResultState_toString[];
+
+@interface QueueIt : RCTEventEmitter <RCTBridgeModule, QueuePassedDelegate, QueueViewWillOpenDelegate, QueueDisabledDelegate, QueueITUnavailableDelegate, QueueUserExitedDelegate>
 
 @end
--- ios/QueueIt.m
+++ ios/QueueIt.m
@@ -1,20 +1,36 @@
 #import "QueueIt.h"
 
+@interface QueueIt ()
+@property (nonatomic, strong)QueueITEngine* engine;
+@property RCTPromiseResolveBlock resolve;
+@property RCTPromiseRejectBlock reject;
+@end
+
+NSString * const EnqueueResultState_toString[] = {
+    [Passed] = @"Passed",
+    [Disabled] = @"Disabled",
+    [Unavailable] = @"Unavailable"
+};
+
+UIViewController *__nullable RCTPresentedViewController(void)
+{
+  if (RCTRunningInAppExtension()) {
+    return nil;
+  }
+
+  UIViewController *controller = RCTKeyWindow().rootViewController;
+  UIViewController *presentedController = controller.presentedViewController;
+  while (presentedController && ![presentedController isBeingDismissed]) {
+    controller = presentedController;
+    presentedController = controller.presentedViewController;
+  }
+
+  return controller;
+}
+
 @implementation QueueIt
 
 RCT_EXPORT_MODULE()
-
-// Example method
-// See // https://facebook.github.io/react-native/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-{
-  NSNumber *result = @([a floatValue] * [b floatValue]);
-
-  resolve(result);
-}
 
 RCT_REMAP_METHOD(enableTesting, enableTesting)
 {
@@ -22,8 +38,72 @@ RCT_REMAP_METHOD(enableTesting, enableTesting)
 }
 
 RCT_REMAP_METHOD(runAsync,
-                 runAsync:(nonnull NSString*) clientId eventOrAliasId:(nonnull NSString*)eventOrAliasId){
+                 runAsync:(nonnull NSString*) customerId
+                 eventOrAliasId:(nonnull NSString*) eventOrAliasId
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject){
     NSLog(@"Running");
+    NSString* layoutName = nil; // Optional (pass nil if no layout specified)
+    NSString* language = nil; // Optional (pass nil if no language specified)
+
+    UIViewController* vc = RCTPresentedViewController();
+    self.engine = [[QueueITEngine alloc]initWithHost:vc customerId:customerId eventOrAliasId:eventOrAliasId layoutName:layoutName language:language];
+    //[self.engine setViewDelay:5]; // Optional delay parameter you can specify (in case you want to inject some animation before Queue-It UIWebView or WKWebView will appear
+    self.engine.queuePassedDelegate = self; // Invoked once the user is passed the queue
+    self.engine.queueViewWillOpenDelegate = self; // Invoked to notify that Queue-It UIWebView or WKWebview will open
+    self.engine.queueDisabledDelegate = self; // Invoked to notify that queue is disabled
+    self.engine.queueITUnavailableDelegate = self; // Invoked in case QueueIT is unavailable (500 errors)
+    self.engine.queueUserExitedDelegate = self; // Invoked when user chooses to leave the queue
+    self.resolve = resolve;
+    self.reject = reject;
+    // run the engine
+
+    NSError* error = nil;
+    @try {
+        BOOL success = [self.engine run:&error];
+        if (!success) {
+            if ([error code] == NetworkUnavailable) {
+                // Thrown when Queue-It detects no internet connectivity
+                NSLog(@"%ld", (long)[error code]);
+                NSLog(@"Network unavailable was caught in DetailsViewController");
+                NSLog(@"isRequestInProgress - %@", self.engine.isRequestInProgress ? @"YES" : @"NO");
+                resolve(@{@"Token": @"", @"Status": ENQUEUE_STATE(Unavailable)});
+            }
+            else if ([error code] == RequestAlreadyInProgress) {
+                // Thrown when request to Queue-It has already been made and currently in progress. In general you can ignore this.
+            }
+            else {
+                reject(@"error", @"Unknown error was returned by QueueITEngine", error);
+            }
+        }
+    } @catch (NSException *exception) {
+        reject(@"error", @"Unknown unhandled exception by QueueITEngine", error);
+    }
+}
+
+- (NSArray<NSString *> *)supportedEvents
+{
+  return @[@"openingQueueView"];
+}
+
+- (void)notifyYourTurn:(QueuePassedInfo *)queuePassedInfo {
+    self.resolve(@{@"Token": queuePassedInfo.queueitToken, @"Status": ENQUEUE_STATE(Passed)});
+}
+
+- (void)notifyQueueViewWillOpen {
+    [self sendEventWithName:@"openingQueueView" body:@{}];
+}
+
+- (void)notifyQueueDisabled {
+    self.resolve(@{@"Token": @"", @"Status": ENQUEUE_STATE(Disabled)});
+}
+
+- (void)notifyQueueITUnavailable:(NSString *)errorMessage {
+    self.resolve(@{@"Token": @"", @"Status": ENQUEUE_STATE(Unavailable)});
+}
+
+- (void)notifyUserExited {
+    NSLog(@"User exited app.");
 }
 
 @end
